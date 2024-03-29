3dae9fcab5cfe8081cb6e9e1e1d54e69726523a5
SureshReddy <reddysureshreddy84@gmail.com>
2024-03-29 17:42:40 +0530

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -9,9 +8,8 @@ import { AllBooksComponent } from './all-books/all-books.component';
 import { HttpClientModule } from '@angular/common/http';
 import { MaterialModule } from './Material.module';
 import { SelectedBookToBuyComponent } from './BuyBook/selected-book-to-buy/selected-book-to-buy.component';
-import {MatFormFieldModule} from '@angular/material/form-field';
-
-
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon'; // Add this import
 
 @NgModule({
   declarations: [
@@ -19,17 +17,18 @@ import {MatFormFieldModule} from '@angular/material/form-field';
     HeaderComponent,
     MenuComponent,
     AllBooksComponent,
+    SelectedBookToBuyComponent, // Make sure to include your component here
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     MaterialModule,
-    MatFormFieldModule
+    MatFormFieldModule,
+    MatIconModule, // Add MatIconModule to the imports array
   ],
- 
   providers: [],
   bootstrap: [AppComponent],
-  entryComponents:[SelectedBookToBuyComponent]
+  entryComponents: [SelectedBookToBuyComponent]
 })
 export class AppModule { }
