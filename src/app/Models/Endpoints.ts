1370249a4607cf230fdb3c995effc05c1caecfcf
sureshreddy2 <159027576+sureshreddy2@users.noreply.github.com>
2024-07-09 19:44:57 +0530

    Update Endpoints.ts

--- src/app/Models/Endpoints.ts
+++ src/app/Models/Endpoints.ts
@@ -1,5 +1,5 @@
 export class Endpoints{
-  public  BookListURl='https://65.2.175.13:80'
+  public  BookListURl='https://65.2.175.13:8080'
   public  GetBooks='/api/GetArchitetureBookType'
   public  GetBookType='/api/GetArtsArchitetureType'
 }
