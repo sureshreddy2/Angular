742fd382c3c6e317bb5af2d8bfc57dbff8a6d49b
sureshreddy2 <159027576+sureshreddy2@users.noreply.github.com>
2024-07-10 12:09:30 +0530

    Add files via upload

---
+++ src/enivironments/environment.prod.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: true,
+  apiUrl: 'http://3.110.119.124:8080' // replace with your backend URL
+};
+
+
+
+
+
+
---
+++ src/enivironments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://3.110.119.124:8080' // replace with your backend URL
+};
+
